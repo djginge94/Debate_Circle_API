c8284e6a3e41ef38f07b7c377f60a931d7b7d9b5
Joshua Colley <>
2020-03-03 19:04:28 +0000

    update auth middleware

--- functions/api/Services/auth.js
+++ functions/api/Services/auth.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 module.exports = (req, res, next) => {
-    const idToken = req.header('Authentocation');
+    const idToken = req.header('Authentication');
     admin.auth().verifyIdToken(idToken)
         .then(decodedToken => {
             return next();
