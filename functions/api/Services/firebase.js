aeee1af49e422fe5cbf5a36f72cfd621ff021536
Joshua Colley <>
2020-03-07 18:13:32 +0000

    update firebase service for release

--- functions/api/Services/firebase.js
+++ functions/api/Services/firebase.js
@@ -1,13 +1,13 @@
 const admin = require("firebase-admin");
 const functions = require('firebase-functions');
-// admin.initializeApp(functions.config().default);
+admin.initializeApp(functions.config().default);
 
-var serviceAccount = require("../../.config/debate-circle-adc5e-firebase-adminsdk-cil0t-eeb800cf4e.json");
+// var serviceAccount = require("../../.config/debate-circle-adc5e-firebase-adminsdk-cil0t-eeb800cf4e.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://debate-circle-adc5e.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://debate-circle-adc5e.firebaseio.com"
+// });
 
 exports.admin = admin;
 exports.debate_database = admin.firestore().collection("Debates");
